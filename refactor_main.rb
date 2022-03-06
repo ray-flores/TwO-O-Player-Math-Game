99805e7f5fe067a4f19faea99d8b0f1cf60f3eb3
Ray Flores <rayflores@Rays-MBP.ht.home>
2022-03-06 00:43:48 -0500

    Use while loop to create question rounds

--- refactor_main.rb
+++ refactor_main.rb
@@ -6,5 +6,25 @@ require './class_question'
 @turn = false
 
 while (player1.life_points > 0 && player2.life_points > 0) do
-  
+  player = (turn == false) ? player1 : player2
+  q = Question.new
+  puts "#{player}: " + q.prompt
+  print "> "
+  choice = gets.chomp
+  if choice.to_i == q.ans
+    puts "YES! You are correct"
+    puts "P1: #{player1.life_points}/3 vs P2: #{player2.life_points}/3"
+  else
+    player.life_points -= 1
+    puts "#{player}: Seriously? No!"
+    puts "P1: #{player1.life_points}/3 vs P2: #{player2.life_points}/3"
+  end
+  turn = true
+  puts "---NEW TURN---"
+end
+
+if (player.life_points == 0)
+  puts "#{player}"
+
+
 end
