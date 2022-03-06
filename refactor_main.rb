4930e5d4ad809162ca404643dad0469fca1529eb
Ray Flores <rayflores@Rays-MBP.ht.home>
2022-03-06 02:23:59 -0500

    Add game over condition

--- refactor_main.rb
+++ refactor_main.rb
@@ -5,26 +5,31 @@ require './class_question'
 @player2 = Player.new(2)
 @turn = false
 
-while (player1.life_points > 0 && player2.life_points > 0) do
-  player = (turn == false) ? player1 : player2
+while (@player1.life_points > 0 && @player2.life_points > 0) do
+  player = (@turn == false) ? @player1 : @player2
   q = Question.new
-  puts "#{player}: " + q.prompt
+  puts "---NEW TURN---"
+  puts "#{player.name}: " + q.prompt
   print "> "
   choice = gets.chomp
   if choice.to_i == q.ans
     puts "YES! You are correct"
-    puts "P1: #{player1.life_points}/3 vs P2: #{player2.life_points}/3"
+    puts "P1: #{@player1.life_points}/3 vs P2: #{@player2.life_points}/3"
   else
     player.life_points -= 1
-    puts "#{player}: Seriously? No!"
-    puts "P1: #{player1.life_points}/3 vs P2: #{player2.life_points}/3"
+    puts "#{player.name}: Seriously? No!"
+    puts "P1: #{@player1.life_points}/3 vs P2: #{@player2.life_points}/3"
   end
-  turn = true
-  puts "---NEW TURN---"
+  if player == @player1
+    @turn = true
+  elsif
+    @turn = false
+  end
 end
 
-if (player.life_points == 0)
-  puts "#{player}"
-
-
+if (@player1.life_points == 0 && player = @player2 || @player2.life_points == 0 && player = @player1) 
+  puts "#{player.name} wins with a score of #{player.life_points}/3"
+  puts "---GAME OVER---"
+  puts "Goodbye!"
+  return;
 end
