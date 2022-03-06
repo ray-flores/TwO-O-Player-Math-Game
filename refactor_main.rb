a825640ca75d6a95f275db9349e69494f9b82aa3
Ray Flores <rayflores@Rays-MBP.ht.home>
2022-03-05 20:08:53 -0500

    Setup refactor

---
+++ refactor_main.rb
@@ -0,0 +1,10 @@
+require './class_player'
+require './class_question'
+
+@player1 = Player.new(1)
+@player2 = Player.new(2)
+@turn = false
+
+while (player1.life_points > 0 && player2.life_points > 0) do
+  
+end
